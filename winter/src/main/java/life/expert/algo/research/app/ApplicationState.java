e0074cd93a7cf0ae169577f1bff8ce7ea316e64f
wilmer krisp <wilmerkrisp@yahoo.com>
2019-07-26 14:40:43 +0300

    new template for spring app added

---
+++ winter/src/main/java/life/expert/algo/research/app/ApplicationState.java
@@ -0,0 +1,12 @@
+package life.expert.algo.research.app;
+//@Header@
+//--------------------------------------------------------------------------------
+//
+//                          winter  life.expert.algo.research.app
+//                           wilmer 2019/07/25
+//
+//--------------------------------------------------------------------------------
+
+public class ApplicationState
+	{
+	}
---
+++ winter/src/testFunctional/java/life/expert/algo/research/TestApplicationConfiguration.java
@@ -0,0 +1,12 @@
+package life.expert.algo.research;
+//@Header@
+//--------------------------------------------------------------------------------
+//
+//                          winter  life.expert.algo.research
+//                           wilmer 2019/07/25
+//
+//--------------------------------------------------------------------------------
+
+public class TestApplicationConfiguration
+	{
+	}
